d619f6d63821ca39c5ec0151f105e9f51218cca7
DanielAntonioSalcidoGutierrez19 <a222214743@unison.mx>
2024-02-08 09:49:01 -0700

    first commit

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+</head>
+
+<body>
+    <h1> Sistema Escolar </h1>
+    <!-- POST / GET -->
+<form action="validar.php" method="GET">
+
+<Label for="user">Usuario: </Label>
+<input type="text" name="user" required>
+<br>
+<Label for="pswd">Contraseña: </Label>
+<input type="password" name="pswd"required>
+<br>
+<button type="sumbit">padentro </button>
+</form>
+</body>
+</html>
